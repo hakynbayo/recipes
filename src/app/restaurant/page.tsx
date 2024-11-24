9c3a183b4142bdb85443215178dffb3776a2010d
hakynbayo <akinbayo.akinwande@decagon.dev>
2024-11-24 21:34:34 +0100

    build

--- src/app/restaurant/page.tsx
+++ src/app/restaurant/page.tsx
@@ -84,7 +84,7 @@ const RestaurantsPage: React.FC = () => {
 
       <button
         onClick={fetchRestaurants}
-        className="bg-purple-500 text-white px-4 py-2 rounded-md"
+        className="bg-purple-500 text-white px-6 py-2 rounded-md"
       >
         Get Nearby Restaurants
       </button>
